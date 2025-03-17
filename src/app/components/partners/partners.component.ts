55174a7c387dcdddea02904dda03ffe5f4553e00
luceva2002 <luceva2002@gmail.com>
2025-03-17 02:11:48 +0100

    scrolltrigger su partners

--- src/app/components/partners/partners.component.ts
+++ src/app/components/partners/partners.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 interface Partner {
   name: string;
@@ -14,8 +18,48 @@ interface Partner {
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.scss']
 })
-export class PartnersComponent {
+export class PartnersComponent implements AfterViewInit {
   partners: Partner[] = [
+    {
+      name: 'Fendi',
+      logo: 'assets/images/partners/fendi.svg',
+      description: 'Collaborazione per soluzioni digitali nel settore luxury'
+    },
+    {
+      name: 'Grandi Stazioni',
+      logo: 'assets/images/partners/grandi-stazioni.svg',
+      description: 'Sviluppo di sistemi di gestione infrastrutturale'
+    },
+    {
+      name: 'RGI',
+      logo: 'assets/images/partners/rgi.svg',
+      description: 'Soluzioni software per il settore assicurativo'
+    },
+    {
+      name: 'Antin',
+      logo: 'assets/images/partners/antin.svg',
+      description: 'Consulenza IT per il settore finanziario'
+    },
+    {
+      name: 'Fendi',
+      logo: 'assets/images/partners/fendi.svg',
+      description: 'Collaborazione per soluzioni digitali nel settore luxury'
+    },
+    {
+      name: 'Grandi Stazioni',
+      logo: 'assets/images/partners/grandi-stazioni.svg',
+      description: 'Sviluppo di sistemi di gestione infrastrutturale'
+    },
+    {
+      name: 'RGI',
+      logo: 'assets/images/partners/rgi.svg',
+      description: 'Soluzioni software per il settore assicurativo'
+    },
+    {
+      name: 'Antin',
+      logo: 'assets/images/partners/antin.svg',
+      description: 'Consulenza IT per il settore finanziario'
+    },
     {
       name: 'Fendi',
       logo: 'assets/images/partners/fendi.svg',
@@ -37,4 +81,26 @@ export class PartnersComponent {
       description: 'Consulenza IT per il settore finanziario'
     }
   ];
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.initHorizontalScroll();
+  }
+
+  private initHorizontalScroll(): void {
+    const partnersGrid = this.el.nativeElement.querySelector('#partnersGrid');
+    gsap.to(partnersGrid, {
+      x: () => -(partnersGrid.scrollWidth - partnersGrid.clientWidth) + 'px',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: partnersGrid,
+        start: 'top center',
+        end: () => "+=" + (partnersGrid.scrollWidth - partnersGrid.clientWidth),
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1
+      }
+    });
+  }
 } 
